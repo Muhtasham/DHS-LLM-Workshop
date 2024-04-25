14a5ec1f7baba3bd0b6090a7d8641e25ebe54e63
muhtasham <muhtasham97@gmail.com>
2024-04-25 23:22:40 +0200

    Update train.py

--- chat_assistant/sft/training/train.py
+++ chat_assistant/sft/training/train.py
@@ -12,6 +12,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+os.environ["WANDB_PROJECT"] = "llama3-continual"
+
+
 from dataclasses import dataclass, field
 import os
 import sys
